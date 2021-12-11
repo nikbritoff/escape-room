a0003f591f8b66cbd4ddda9a380b19d0e747f00c
nikbritoff <britov.nd@gmail.com>
2021-12-11 14:08:51 +0300

    Вносит правки

--- src/components/not-found/not-found.jsx
+++ src/components/not-found/not-found.jsx
@@ -1,26 +1,7 @@
 import Footer from 'components/common/footer/footer';
 import Header from 'components/common/header/header';
-// import { Link } from 'react-router-dom';
-// import styles from './not-found.module.css';
+import { AppRoute } from 'components/const';
 import * as S from './not-found.styled';
-
-
-// function NotFound() {
-//   return (
-//     <div className={styles['page--not-found']}>
-//       <Header/>
-//       <main className={styles['page__main--not-found']}>
-//         <div className={styles.container}>
-//           <section className={styles['not-found']}>
-//             <h1 className={styles['not-found__title']}>404. Page not found</h1>
-//             <Link className={styles['not-found__link']} to="/">Back to the home page</Link>
-//           </section>
-//         </div>
-//       </main>
-//       <Footer/>
-//     </div>
-//   );
-// }
 
 const NotFound = () => (
   <S.Container>
@@ -28,7 +9,7 @@ const NotFound = () => (
     <S.Main>
       <S.Section>
         <S.Title>404. Страница не найдена</S.Title>
-        <S.LinkToMain>Вернуться на главную</S.LinkToMain>
+        <S.LinkToMain to={AppRoute.Home}>Вернуться на главную</S.LinkToMain>
       </S.Section>
     </S.Main>
     <Footer/>

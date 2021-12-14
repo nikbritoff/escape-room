66145bf4850f8d1cf62435c2ed877667f2c7d7ae
nikbritoff <britov.nd@gmail.com>
2021-12-14 20:48:01 +0300

    Исправляет изменение экранов

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -4,19 +4,31 @@ import {
   PageHeading,
   PageSubtext,
 } from 'components/common/common';
+import { Screen } from 'const';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { changeScreen } from 'store/action';
 import { QuestsCatalog } from './components/components';
 import * as S from './home.styled';
 
-const HomePage = () => (
-  <MainLayout>
-    <S.Main forwardedAs="main">
-      <PageHeading>
-        <PageTitle>Выберите тематику</PageTitle>
-        <PageSubtext>квесты в Санкт-Петербурге</PageSubtext>
-      </PageHeading>
-      <QuestsCatalog />
-    </S.Main>
-  </MainLayout>
-);
+const HomePage = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(changeScreen(Screen.Quests));
+  });
+
+  return (
+    <MainLayout>
+      <S.Main forwardedAs="main">
+        <PageHeading>
+          <PageTitle>Выберите тематику</PageTitle>
+          <PageSubtext>квесты в Санкт-Петербурге</PageSubtext>
+        </PageHeading>
+        <QuestsCatalog />
+      </S.Main>
+    </MainLayout>
+  );
+};
 
 export default HomePage;
